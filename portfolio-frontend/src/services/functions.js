f0f7a7afb553083f46e113b53882be1cf196bf8b
john-craig <john.craig1@marist.edu>
2021-11-18 11:38:51 -0500

    Automated commit on Thu Nov 18 11:38:51 AM EST 2021

--- portfolio-frontend/src/services/functions.js
+++ portfolio-frontend/src/services/functions.js
@@ -1,4 +1,5 @@
-const API_URL="http://localhost:8282"
+//const API_URL="http://localhost:8282"
+const API_URL="http://backend:8282"
 //const API_URL = process.env.BACKEND_IP + "8282";
 //const API_URL = "/api"
 
